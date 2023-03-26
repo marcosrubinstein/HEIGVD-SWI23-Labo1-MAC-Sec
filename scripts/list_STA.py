bbe7e93bfa21561572820cf680b1c9f3f46353ed
jdoe <anthony.coke@heig-vd.ch>
2023-03-26 17:54:39 +0200

    Update list_STA script

--- scripts/list_STA.py
+++ scripts/list_STA.py
@@ -5,10 +5,8 @@ parser.add_argument("-i", required=True, help="Interface to use")
 parser.add_argument("-s", required=True, help="SSID to search")
 args = parser.parse_args()
 
-
 sta_list = []
 def sta_finder(pkt):
-    # Le paquet doit être un probe + être le SSID que l'on cherche + ne pas faire déjà partie de la liste (ne liste pas 2 fois le même)
     if pkt.type == 0 and pkt.subtype == 4 and args.s == pkt.info.decode() and pkt.addr2 not in sta_list:
         sta_list.append(pkt.addr2)
         print(pkt.addr2)
