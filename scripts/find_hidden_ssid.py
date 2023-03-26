568bd718103e7aa2ff5c0bd4682b994968c106ed
Mbayo <gmbayo11@gmail.com>
2023-03-26 21:49:58 +0200

    Upgrade fin_hidden_ssid.py to hope between channels

--- scripts/find_hidden_ssid.py
+++ scripts/find_hidden_ssid.py
@@ -2,7 +2,7 @@ from scapy.all import *
 import os
 
 # Define the filter to capture only Wi-Fi packets
-wifi_filter = "wlan"
+wifi_filter = "wlan type mgt"
 
 # Configure the network interface in monitor mode
 interface = "wlan0mon"
@@ -10,15 +10,24 @@ os.system(f"sudo ip link set {interface} down")
 os.system(f"sudo iw dev {interface} set type monitor")
 os.system(f"sudo ip link set {interface} up")
 
-# Capture Wi-Fi packets for 10 seconds
-packets = sniff(iface=interface, timeout=15)
 
-# Analyze each packet to extract the SSID
-for packet in packets:
-    if packet.haslayer(Dot11Elt) and packet[Dot11Elt].ID == 0:
-        bssid = packet.addr3
-        ssid = packet[Dot11Elt].info.decode()
-        print(f"BSSID: {bssid}, SSID: {ssid}")
+# set the list of Wi-Fi channels to scan
+channels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+for channel in channels:
+    os.system(f"sudo iwconfig {interface} channel {channel}")
+    print(f"Scanning channel {channel}...")
+
+    packets = sniff(filter=wifi_filter, iface=interface, timeout=5)
+
+    # Analyze each packet to extract the SSID
+    for packet in packets:
+        if packet.haslayer(Dot11Elt) and packet[Dot11Elt].ID == 0:
+            bssid = packet.addr3
+            addr1 = packet.addr1
+            addr2 = packet.addr2
+            ssid = packet[Dot11Elt].info.decode()
+            print(f"BSSID: {bssid}, RA/DA: {addr1}, TA/SA: {addr2}, SSID: {ssid}")
 
 print(f"end of script")
 # Restore the network interface to its original state
