10b44d0fb2df436d44f02af879f0f7e703dda78d
David Gallay <david.gallay@heig-vd.ch>
2023-03-30 14:45:03 +0200

    Done script for ex4

--- scripts/ex_4_ssid_detection.py
+++ scripts/ex_4_ssid_detection.py
@@ -2,4 +2,63 @@
 
 # Source: https://gist.github.com/securitytube/5291959
 
-# TODO : same as ex_1_2 but with probe requests 
+# TODO : same as ex_1_2 but with probe requests
+
+from swi_utils import (
+    find_ap_by_probe_request_timeout,
+    find_ap,
+    ask_ap_to_spoof,
+    DEFAULT_IFACE
+)
+
+# ====================================================================================================
+# START of script parameters management
+
+import argparse
+def handle_arguments():
+    # Argument parsing
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--interface")
+    parser.add_argument("-c", "--channels")
+    parser.add_argument("-p", "--packet-count")
+    args = parser.parse_args()
+
+    # Variables to contain argument values
+    interface = ""
+    packet_count = ""
+
+    # Get inteface name from args
+    if args.interface == None:
+        print("[WARN] No interface specified, defaulting to wlan0mon")
+        interface = DEFAULT_IFACE
+    else:
+        interface = args.interface
+
+    # Get packet_count for sniffing duration from args
+    if args.packet_count == None:
+        print("[WARN] No packet count for sniffing defined, defaulting to 10")
+        packet_count = 10
+    else:
+        packet_count = int(args.packet_count)
+    channels = args.channels or None
+    if channels:
+        channels = list({int(i) for i in channels.split(",")})
+    return interface, packet_count, channels
+
+# END of script parameters management
+# ====================================================================================================
+
+def main(limit_to_local=False):
+    interface, packet_count, channels = handle_arguments()
+
+    ap_list = find_ap_by_probe_request_timeout()
+    if limit_to_local:
+        local_ap = find_ap()
+        ap_list = list(set(ap_list).intersection(set(local_ap)))
+    target = ask_ap_to_spoof(ap_list)
+    target.spoof(interface, spoof_mac=True)
+
+
+
+if __name__ == "__main__":
+    main()
