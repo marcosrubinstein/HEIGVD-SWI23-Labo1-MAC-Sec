f107c73223ebd084db2c591db1ba171f4b305525
Pascal Perrenoud <pascal@pband.ch>
2023-04-02 22:37:43 +0200

    Reduce to one SSID per AP

--- scripts/sta-ap-association.py
+++ scripts/sta-ap-association.py
@@ -10,12 +10,9 @@ def ap_pckts(pkt):
     # Using the Beacon Announce or the Prob Response, we can list the APs around
     if pkt.haslayer(Dot11Beacon) or pkt.haslayer(Dot11ProbeResp):
         ap_mac = pkt.addr2  # The address 2 is the MAC of the AP
-        # If we didn't see it yet, we create a new list
-        if ap_mac not in aps:
-            aps[ap_mac] = []
-        # Then we save the announced SSID with the mac
+        # Saving the announced SSID with the mac
         ssid = pkt.info.decode()
-        aps[ap_mac].append(ssid)
+        aps[ap_mac] = ssid
 
 
 # Start sniffing for APs
@@ -26,12 +23,11 @@ sniff(iface=ifname, prn=ap_pckts, store=False)
 def sta_pckts(pkt):
     # Using Probe and Association requests
     if pkt.haslayer(Dot11ProbeReq) or pkt.haslayer(Dot11AssoReq):
-        sta_mac = pkt.addr2  #  Get the STA's MAC
-        ap_mac = pkt.addr3  #  Get the AP's MAc
-        # If the AP is in the AP list, show all associated SSIDs
+        sta_mac = pkt.addr2  # Get the STA's MAC
+        ap_mac = pkt.addr3  # Get the AP's MAc
+        # If the AP is in the AP list, show associated SSID
         if ap_mac in aps:
-            for ssid in aps[ap_mac]:
-                print(f"AP: {ap_mac} ({ssid}) - STA: {sta_mac}")
+            print(f"AP: {ap_mac} ({aps[ap_mac]}) - STA: {sta_mac}")
 
 
 # Start sniffing for STAs
